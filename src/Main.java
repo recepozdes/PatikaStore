c3f8a641b32e32941680fcb84f64341b664871e9
RecepOzdes <recepozdes@gmail.com>
2022-07-25 09:29:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        //https://github.com/AlpKarar/Java102-PatikaStore/blob/main/Brand.java
-        //https://github.com/KaanSenel/PatikaStore/blob/master/src/Store.java
-        //https://github.com/furkanyazar/PatikaStore/blob/master/src/BrandManager.java
-        //https://github.com/Mmusagezer/PatikaStore/blob/master/src/com/patikastore/Brand.java
-        //https://github.com/henesacar/PatikaStore/blob/master/src/Brand.java
-        //https://github.com/ozanbaskan/patikaStore_java/blob/main/src/Brand.java
+       
         Scanner sc = new Scanner(System.in);
         Store store = new Store("Patika Store");
         store.menu();
